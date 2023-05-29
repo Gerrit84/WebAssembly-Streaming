35730b03f1cc5fa04feabfd56b23d29804537b25
Gerrit84 <gerritgauder@hotmail.com>
2023-05-29 13:36:18 +0200

    Initial push

---
+++ hello.c
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2011 The Emscripten Authors.  All rights reserved.
+ * Emscripten is available under two separate licenses, the MIT license and the
+ * University of Illinois/NCSA Open Source License.  Both these licenses can be
+ * found in the LICENSE file.
+ */
+
+#include <emscripten.h>
+
+EMSCRIPTEN_KEEPALIVE
+int calc(int a, int b)
+{
+  return a + b;
+}
+
+/*EMSCRIPTEN_KEEPALIVE
+int main() {
+  printf("hello, world!\n");
+  return 0;
+}*/
